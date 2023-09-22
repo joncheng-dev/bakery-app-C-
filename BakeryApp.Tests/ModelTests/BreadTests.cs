823cb9f036d12179479ce1f48269d92e88a9fdcf
Jonathan Cheng <jcheng461@gmail.com>
2023-09-22 09:37:03 -0700

    write & pass test1 NumOfLoaves get action; update constructor

--- BakeryApp.Tests/ModelTests/BreadTests.cs
+++ BakeryApp.Tests/ModelTests/BreadTests.cs
@@ -9,8 +9,17 @@ namespace BakeryApp.Tests
     [TestMethod]
     public void BreadConstructor_CreatesInstanceOfBread_Bread()
     {
-      Bread anOrderOfBread = new Bread();
+      Bread anOrderOfBread = new Bread(2);
       Assert.AreEqual(typeof(Bread), anOrderOfBread.GetType());
+    }
+
+    [TestMethod]
+    public void GetNumberOfLoaves_ReturnsNumberOfLoaves_Int()
+    {
+      int numOrdered = 2;
+      Bread anOrderOfBread = new Bread(numOrdered);
+      int resultOfGetAction = anOrderOfBread.NumOfLoaves;
+      Assert.AreEqual(numOrdered, resultOfGetAction);
     }
 
     // [TestMethod]
--- BakeryApp/Models/Bread.cs
+++ BakeryApp/Models/Bread.cs
@@ -2,6 +2,11 @@ namespace BakeryApp.Models
 {
   public class Bread
   {
-    
+    public int NumOfLoaves { get; }
+
+    public Bread(int userQuantity)
+    {
+      NumOfLoaves = userQuantity;
+    }
   }
 }
